fdda06a046baabd65a516607b482fbff9f3ddc8e
Ben Keller <bjkeller@uw.edu>
2020-06-01 08:43:27 -0700

    Add docstrings for path functions

--- pxfish/paths.py
+++ pxfish/paths.py
@@ -1,4 +1,6 @@
-"""Functions for creating directories and directory paths"""
+"""
+Utilities for creating paths and directories
+"""
 
 import os
 import re
@@ -29,10 +31,36 @@ def simplename(name):
 
 
 def create_named_path(path, name):
+    """
+    Create a path for a named object (operation type/library).
+    Joins the canonical form of the object name to the base path.
+    The canonical name is created using the simplename function.
+
+    Note: does not create the directory.
+
+    Arguments:
+      path (string): the base path
+      name (string): the name of the object
+
+    Returns:
+      string: the path of the named object
+    """
     return os.path.join(path, simplename(name))
 
 
 def create_library_path(category_path, library_name):
+    """
+    Create a path for a library within the directory for a category.
+
+    Note: does not create the directory.
+
+    Arguments:
+      category_path (string): the path for the category
+      library_name (string): the name of the library
+
+    Returns:
+      string: the path of the library
+    """
     return create_named_path(
         os.path.join(category_path, 'libraries'),
         library_name
@@ -40,6 +68,18 @@ def create_library_path(category_path, library_name):
 
 
 def create_operation_path(category_path, operation_type_name):
+    """
+    Create a path for an operation type within the directory for a category.
+
+    Note: does not create the directory.
+
+    Arguments:
+      category_path (string): the path for the category
+      operation_type_name (string): the name of the operation type
+
+    Returns:
+      string: the path of the operation type
+    """
     return create_named_path(
         os.path.join(category_path, 'operation_types'),
         operation_type_name
