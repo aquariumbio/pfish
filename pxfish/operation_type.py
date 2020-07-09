0d565a52856e70c2b987936dbc478250c65ef186
Amy Cash <amycash@uw.edu>
2020-07-09 12:48:16 -0700

    Remove uneeded pull function from ot

--- pxfish/operation_type.py
+++ pxfish/operation_type.py
@@ -110,22 +110,6 @@ def write_operation_type(path, operation_type):
     )
 
 
-def pull(path, operation_types=[], libraries=[]):
-    """
-    Pulls OperationType and/or Library files from the Aquarium instance.
-
-    Arguments:
-        path (String): the path for the directory where files should be written
-        operation_types (List): list of OperationTypes whose files to pull
-        libraries (List): list of Libraries whose files to pull
-    """
-    for operation_type in operation_types:
-        write_operation_type(path, operation_type)
-
-    for library in libraries:
-        write_library(path, library)
-
-
 def operation_type_code_names():
     return ['protocol', 'precondition', 'cost_model', 'documentation', 'test']
 
