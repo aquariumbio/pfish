0039ec2a1a89585987cfc5892713cf4ef778ab99
Ben Keller <bjkeller@uw.edu>
2020-07-15 21:11:59 +0000

    Reorganize push code

--- pxfish/library.py
+++ pxfish/library.py
@@ -2,7 +2,7 @@
 """
 Functions for pushing, pulling, and creating Library files
 """
-
+import code
 import json
 import logging
 import os
@@ -12,9 +12,6 @@ from paths import (
     create_operation_path,
     create_library_path,
     makedirectory
-)
-from code import (
-    write_code
 )
 from definition import (
     write_library_definition_json
@@ -92,7 +89,7 @@ def write_files(path, library):
         )
     file_name = 'source.rb'
     try:
-        write_code(library_path, file_name, code_object)
+        code.write(library_path, file_name, code_object)
     except OSError as error:
         logging.warning("Error {} writing file {} for library {}".format(
             error, file_name, library.name))
@@ -149,43 +146,33 @@ def push(aq, directory_path, component_names):
     with open(os.path.join(directory_path, 'definition.json')) as f:
         definitions = json.load(f)
 
-    for name in component_names:
-        file_name = "{}.rb".format(name)
-        try:
-            with open(os.path.join(directory_path, file_name)) as f:
-                read_file = f.read()
+    user_id = aq.User.where({"login": aq.login})
+    query = {
+        "category": definitions['category'],
+        "name": definitions['name']
+    }
+    if definitions['parent_class'] == 'Library':
+        parent_object = aq.Library.where(query)
+        parent_type_name = 'library'
+    elif definitions['parent_class'] == 'OperationType':
+        parent_object = aq.OperationType.where(query)
+        parent_type_name = 'operation type'
 
-        # TODO: create test file if it doesn't exist?
-
-        except FileNotFoundError as error:
-            logging.warning(
-                "Error {} writing file {} file does not exist".format(
-                    error, file_name))
-            return
-
-        user_id = aq.User.where({"login": aq.login})
-
-        query = {
-            "category": definitions['category'],
-            "name": definitions['name']
-        }
-        if name == 'source':
-            parent_object = aq.Library.where(query)
-            parent_type_name = 'library'
-        else:
-            parent_object = aq.OperationType.where(query)
-            parent_type_name = 'operation type'
-
-        if not parent_object:
-            logging.warning(
-                "No {} {}/{} on {}".format(
-                    parent_type_name,
-                    definitions['category'],
-                    definitions['name'],
-                    # TODO: make the following specific to user instance
-                    "Aquarium instance"
-                )
+    if not parent_object:
+        logging.warning(
+            "No {} {}/{} on {}".format(
+                parent_type_name,
+                definitions['category'],
+                definitions['name'],
+                # TODO: make the following specific to user instance
+                "Aquarium instance"
             )
+        )
+        return
+
+    for name in component_names:
+        read_file = code.read(path=directory_path, name=name)
+        if read_file is None:
             return
 
         new_code = aq.Code.new(
--- pxfish/operation_type.py
+++ pxfish/operation_type.py
@@ -2,6 +2,7 @@
 Functions for pushing, pulling, and creating Operation Types in Aquarium.
 """
 
+import code
 import json
 import logging
 import os
@@ -11,8 +12,7 @@ from paths import (
     makedirectory
 )
 from code import (
-    create_code_objects,
-    write_code
+    create_code_objects
 )
 from definition import write_definition_json
 
@@ -73,10 +73,10 @@ def write_files(path, operation_type):
 
     category_path = create_named_path(path, operation_type.category)
     makedirectory(category_path)
-    
+
     path = create_operation_path(category_path, operation_type.name)
     makedirectory(path)
-    
+
     code_names = operation_type_code_names()
 
     for name in code_names:
@@ -90,7 +90,7 @@ def write_files(path, operation_type):
 
         file_name = "{}.rb".format(name)
         try:
-            write_code(path, file_name, code_object)
+            code.write(path, file_name, code_object)
         except OSError as error:
             logging.warning(
                 "Error {} writing file {} for operation type {}".format(
@@ -145,59 +145,49 @@ def create(aq, path, category, operation_type_name):
         precondition=code_objects['precondition'],
         documentation=code_objects['documentation'],
         cost_model=code_objects['cost_model'])
-    new_operation_type.field_types = [] 
+    new_operation_type.field_types = []
     aq.utils.create_operation_type(new_operation_type)
 
 
-def push(aq, directory_path, component_names):
+def push(aq, path, component_names):
     """
     Pushes files to the Aquarium instance
 
     Arguments:
         aq (Session Object): Aquarium session object
-        directory_path (String): Directory where files are to be found
+        path (String): Directory where files are to be found
         component_names (List): List of files to push
     """
-    with open(os.path.join(directory_path, 'definition.json')) as f:
+    with open(os.path.join(path, 'definition.json')) as f:
         definitions = json.load(f)
 
-    for name in component_names:
-        file_name = "{}.rb".format(name)
-        try:
-            with open(os.path.join(directory_path, file_name)) as f:
-                read_file = f.read()
+    user_id = aq.User.where({"login": aq.login})
+    query = {
+        "category": definitions['category'],
+        "name": definitions['name']
+    }
+    if definitions['parent_class'] == 'Library':
+        parent_object = aq.Library.where(query)
+        parent_type_name = 'library'
+    elif definitions['parent_class'] == 'OperationType':
+        parent_object = aq.OperationType.where(query)
+        parent_type_name = 'operation type'
 
-        # TODO: create test file if it doesn't exist?
-
-        except FileNotFoundError as error:
-            logging.warning(
-                "Error {} writing file {} file does not exist".format(
-                    error, file_name))
-            return
-
-        user_id = aq.User.where({"login": aq.login})
-
-        query = {
-            "category": definitions['category'],
-            "name": definitions['name']
-        }
-        if name == 'source':
-            parent_object = aq.Library.where(query)
-            parent_type_name = 'library'
-        else:
-            parent_object = aq.OperationType.where(query)
-            parent_type_name = 'operation type'
-
-        if not parent_object:
-            logging.warning(
-                "No {} {}/{} on {}".format(
-                    parent_type_name,
-                    definitions['category'],
-                    definitions['name'],
-                    # TODO: make the following specific to user instance
-                    "Aquarium instance"
-                )
+    if not parent_object:
+        logging.warning(
+            "No {} {}/{} on {}".format(
+                parent_type_name,
+                definitions['category'],
+                definitions['name'],
+                # TODO: make the following specific to user instance
+                "Aquarium instance"
             )
+        )
+        return
+
+    for name in component_names:
+        read_file = code.read(path=path, name=name)
+        if read_file is None:
             return
 
         new_code = aq.Code.new(
